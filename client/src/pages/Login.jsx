b8c11730af83fe8605f467787e73356b5fbdb438
varun <varunchowdary23@gmail.com>
2025-10-29 14:33:05 +0530

    Made Login and Registration pages responsive & show/hide password in login implemented

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,12 @@
 // Uncontrolled form approach
+import { Link } from 'react-router-dom';
+import { Lineicons } from "@lineiconshq/react-lineicons";
+import { EyeSolid, EyeStroke } from "@lineiconshq/free-icons";
+import { useState } from 'react';
+ 
 const Login = () => {
+
+  const [showPassword, setShowPassword] = useState(false);
   const login = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -8,31 +15,39 @@ const Login = () => {
   };
 
   return (
-    <div className="h-screen flex">
-      <div className="bg-blue-100 h-full w-full flex justify-center items-center">
+    <div className="h-screen flex flex-col-reverse md:flex-row">
+      <div className="bg-blue-100 h-full w-full justify-center items-center hidden md:flex">
         <p className="text-5xl font-bold">LOGIN</p>
       </div>
       <div className="bg-blue-300 h-full w-full flex justify-center items-center">
         <form onSubmit={login}>
-          <div className="bg-white rounded-md flex flex-col gap-2 p-3">
+          <div className="bg-white rounded-md flex flex-col gap-2 p-5">
             <input
               name="username"
-              className="bg-gray-300 p-2 rounded-sm"
+              className="bg-gray-200 p-2 rounded-sm focus:outline-none"
               type="text"
               placeholder="Username"
             />
-            <input
-              name="password"
-              className="bg-gray-300 p-2 rounded-sm"
-              type="text"
-              placeholder="Password"
-            />
-            <button
-              className="bg-blue-200 w-1/2 rounded-sm p-1 cursor-pointer"
-              type="submit"
-            >
-              Login
-            </button>
+            <div className="flex items-center relative">
+              <input
+                name='password'
+                className="bg-gray-200 p-2 rounded-sm focus:outline-none"
+                type={showPassword ? 'text': 'password'}
+                placeholder="Password"
+              />
+              <Lineicons onClick={() => {setShowPassword(!showPassword)}} className='absolute right-2' icon={showPassword ? EyeSolid : EyeStroke} />
+            </div>
+            <div className="flex items-center">
+              <button
+                className="bg-blue-200 w-1/2 rounded-sm p-1 cursor-pointer"
+                type="submit"
+              >
+                Login
+              </button>
+              <Link className="ml-auto text-blue-500" to="/register">
+                Register
+              </Link>
+            </div>
           </div>
         </form>
       </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 // Controlled form approach
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -17,33 +18,38 @@ const Register = () => {
   };
 
   return (
-    <div className="h-screen flex">
-      <div className="bg-blue-100 h-full w-full flex justify-center items-center">
+    <div className="h-screen flex flex-col-reverse md:flex-row">
+      <div className="bg-blue-100 h-full w-full justify-center items-center hidden md:flex">
         <p className="text-5xl font-bold">REGISTER</p>
       </div>
       <div className="bg-blue-300 h-full w-full flex justify-center items-center">
-        <form onSubmit={register} className="flex flex-col gap-2 bg-white p-4">
-          <input
-            name="username"
-            placeholder="Username"
-            value={formData.username}
-            onChange={handleChange}
-            className="bg-gray-200 p-2 rounded-sm"
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            className="bg-gray-200 p-2 rounded-sm"
-          />
-          <button
-            type="submit"
-            className="bg-blue-300 rounded-sm p-2 cursor-pointer"
-          >
-            Register
-          </button>
+        <form onSubmit={register}>
+          <div className="bg-white rounded-md flex flex-col gap-2 p-5">
+            <input
+              name="username"
+              placeholder="Username"
+              value={formData.username}
+              onChange={handleChange}
+              className="bg-gray-200 p-2 rounded-sm"
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              className="bg-gray-200 p-2 rounded-sm"
+            />
+            <div className="flex items-center">
+              <button
+                type="submit"
+                className="bg-blue-200 w-1/2 rounded-sm p-1 cursor-pointer"
+              >
+                Register
+              </button>
+              <Link className="ml-auto text-blue-500" to="/login">Login</Link>
+            </div>
+          </div>
         </form>
       </div>
     </div>
