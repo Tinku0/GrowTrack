63ac8911b73b4cf61ae6ae595332eda8f7955ea4
varun <varunchowdary23@gmail.com>
2025-10-29 15:12:07 +0530

    db conn file moved into config

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { connectToDatabase } = require('./db');
+const { connectToDatabase } = require('./config/db');
 const userRoutes = require('./routes/user');
 const app = express();
 
